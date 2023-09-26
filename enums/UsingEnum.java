07f158dc7138828d3cad186ea0c5964103c4397b
ikmaz1816 <ikmaz1816@gmail.com>
2023-09-26 09:22:26 +0530

    Enums

---
+++ enums/UsingEnum.java
@@ -0,0 +1,9 @@
+package enums;
+
+public class UsingEnum {
+	public static void main(String[] args) {
+		Week monday=Week.Monday;
+		
+		System.out.println(monday);
+	}
+}
---
+++ enums/Week.java
@@ -0,0 +1,13 @@
+package enums;
+
+enum Week {
+	/*
+	 	Enums are final static
+	 	They are used for singleton
+	 */
+	Monday,
+	Tuesday,
+	Wednesday,
+	Thursday,
+	Friday
+}
